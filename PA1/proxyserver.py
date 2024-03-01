33ed3bd82cfc5e1afb2f4cf635f40469486da627
Melina <hyomin.kim@stonybrook.edu>
2024-02-29 22:38:44 -0500

    일단 백업

--- PA1/proxyserver.py
+++ PA1/proxyserver.py
@@ -1,96 +1,64 @@
-import socket
+from socket import *
 import sys
+if len(sys.argv) <= 1:
+    print("Usage : python ProxyServer.py server_ip\n[server_ip : It is the IP Address Of Proxy Server]")
+    sys.exit(2)
 
-def proxy_server(webserver, port, conn, addr, data):
+# Create a server socket, bind it to a port and start listening
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
+tcpSerSock.bind((sys.argv[1], 8888))
+tcpSerSock.listen(5)
+
+while 1:
+    # Start receiving data from the client
+    print('Ready to serve...')
+    tcpCliSock, addr = tcpSerSock.accept()
+    print('Received a connection from:', addr)
+    message = tcpCliSock.recv(1024).decode()
+    print(message)
+    # Extract the filename from the given message
+    print(message.split()[1])
+    filename = message.split()[1].partition("/")[2]
+    print("이거다!!!!!!" + filename)
+    fileExist = "false"
+    filetouse = "/" + filename
+    print(filetouse)
     try:
-        # 연결을 원격 웹 서버로 전달
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((webserver, port))
-        s.send(data)
-
-        while True:
-            # 원격 서버로부터 데이터 받기
-            reply = s.recv(4096)
-
-            if len(reply) > 0:
-                # 데이터를 클라이언트로 전달
-                conn.sendall(reply)
-                # 응답을 캐시에 저장할 수 있도록 추가
-                save_cache(data, reply, webserver, port)
-            else:
-                break
-
-        s.close()
-        conn.close()
-    except socket.error as err:
-        print(err)
-        if s:
-            s.close()
-        if conn:
-            conn.close()
-        sys.exit(1)
-
-def save_cache(request, response, webserver, port):
-    # 여기서는 간단하게 캐시를 파일로 저장합니다.
-    filename = webserver.replace(".", "_") + "_" + str(port) + ".txt"
-    with open(filename, "wb") as cache_file:
-        cache_file.write(request)
-        cache_file.write(response)
-
-def main():
-    try:
-        # 로컬 호스트와 포트 설정
-        host = '127.0.0.1'
-        port = 8888
-
-        # 소켓 생성
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((host, port))
-        s.listen(10)
-
-        print("Proxy server is listening on port", port)
-
-        while True:
-            # 연결 수락
-            conn, addr = s.accept()
-            data = conn.recv(4096)
-            # 클라이언트 요청 분석
-            if not data:
-                continue
+        # Check whether the file exists in the cache
+        f = open(filetouse[1:], "r", encoding='utf-8')
+        outputdata = f.readlines()
+        fileExist = "true"
+        # ProxyServer finds a cache hit and generates a response message
+        tcpCliSock.send("HTTP/1.0 200 OK\r\n".encode())
+        tcpCliSock.send("Content-Type:text/html\r\n".encode())
+        for i in range(0, len(outputdata)):
+            tcpCliSock.send(outputdata[i].encode())
+        print('Read from cache')
+    # Error handling for file not found in cache
+    except IOError:
+        if fileExist == "false":
+            # Create a socket on the proxyserver
+            c = socket(AF_INET, SOCK_STREAM)
+            hostn = filename.replace("www.", "", 1)
+            print(hostn)
             try:
-                first_line = data.decode().split('\n')[0]
-            except UnicodeDecodeError:
-                print("Failed to decode data")
-                continue
-            url = first_line.split(' ')[1]
-            http_pos = url.find("://")
-            if http_pos == -1:
-                temp = url
-            else:
-                temp = url[(http_pos + 3):]
-
-            port_pos = temp.find(":")
-
-            webserver_pos = temp.find("/")
-            if webserver_pos == -1:
-                webserver_pos = len(temp)
-
-            webserver = ""
-            port = -1
-            if port_pos == -1 or webserver_pos < port_pos:
-                port = 80
-                webserver = temp[:webserver_pos]
-            else:
-                port = int((temp[(port_pos + 1):])[:webserver_pos - port_pos - 1])
-                webserver = temp[:port_pos]
-
-            proxy_server(webserver, port, conn, addr, data)
-
-    except Exception as e:
-        print(e)
-        if s:
-            s.close()
-        sys.exit(1)
-
-if __name__ == "__main__":
-    main()
+                # Connect to the socket to port 80
+                c.connect((hostn, 80))
+                print('Socket connected to port 80 of the host')
+                c.sendall(message.encode())
+                buff = c.recv(4096)
+                # Create a new file in the cache for the requested file.
+                # Also send the response in the buffer to client socket and the corresponding file in the cache
+                tmpFile = open("./" + filename, "wb")
+                tmpFile.write(buff)
+                tmpFile.close()
+                tcpCliSock.send(buff)
+            except:
+                print("Illegal request")
+        else:
+            # HTTP response message for file not found
+            tcpCliSock.send("HTTP/1.0 404 sendErrorErrorError\r\n".encode())
+            tcpCliSock.send("Content-Type:text/html\r\n".encode())
+            tcpCliSock.send("\r\n".encode())
+    # Close the client and the server sockets
+    tcpCliSock.close()
